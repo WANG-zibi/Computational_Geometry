db875c0a45096266b0e9be5d6fcb912a922627c8
kingskyuniverse <wtyxxs@foxmail.com>
2020-12-10 20:54:30 +0800

    1

--- p2181.cpp
+++ p2181.cpp
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 100010;
+#define int long long
 int n;
-int main()
+signed main()
 {
     cin >> n;
+    int ans=n * (n-1) / 2 * (n-2) / 3 * (n-3) / 4;
+    cout<<ans<<endl;
+
     return 0;
 }
